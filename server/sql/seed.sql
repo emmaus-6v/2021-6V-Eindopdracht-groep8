d0f41609e61a6d471b61a0b7a21e850024370fea
De-Knokreus-64 <111193@emmauscollege.nl>
2021-02-28 11:40:18 +0000

    aanpassing van seed

--- server/sql/seed.sql
+++ server/sql/seed.sql
@@ -1,2 +1,3 @@
 INSERT INTO baanStatus (status, tijd, opmerking) VALUES (1, CURRENT_TIMESTAMP, "Server is aan")
 INSERT INTO buttonPresses (tijd) VALUES (CURRENT_TIMESTAMP);
+INSERT INTO aantalBalletjes (aantal voorbijgekomen balletjes);
